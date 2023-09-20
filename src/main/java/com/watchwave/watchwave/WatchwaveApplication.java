a2de536193b76cd5e091830ac5c5912046b14383
nadee-darshika <88460737+nadee-darshika@users.noreply.github.com>
2023-09-21 03:41:55 +0530

    created the project

---
+++ src/main/java/com/watchwave/watchwave/WatchwaveApplication.java
@@ -0,0 +1,13 @@
+package com.watchwave.watchwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchwaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchwaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/watchwave/watchwave/WatchwaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watchwave.watchwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchwaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
